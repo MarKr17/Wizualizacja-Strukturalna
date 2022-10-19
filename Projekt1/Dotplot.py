aced00c7119bafa368b734fdbebfc348e47f3f7f
MarKr17 <mariakroczek199@gmail.com>
2022-10-19 14:28:41 +0200

    reading file

--- Projekt1/Dotplot.py
+++ Projekt1/Dotplot.py
@@ -1,9 +1,8 @@
 import os
 os.chdir(os.getcwd())
-print(os.getcwd())
-#file = open("Data/4YCO_D", "r")
-#sequence = file.readline()
-#brackets = file.readline()
+file = open("Data/4YCO_D", "r")
+sequence = file.readline()
+brackets = file.readline()
 
-#print(sequence)
-#print(brackets)
+print(sequence)
+print(brackets)
