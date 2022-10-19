b134711ba2b4ed804c4607b9d57123e46fc28e57
MarKr17 <mariakroczek199@gmail.com>
2022-10-19 14:18:32 +0200

    m

---
+++ Projekt1/Dotplot.py
@@ -0,0 +1,9 @@
+import os
+os.chdir(os.getcwd())
+print(os.getcwd())
+#file = open("Data/4YCO_D", "r")
+#sequence = file.readline()
+#brackets = file.readline()
+
+#print(sequence)
+#print(brackets)
