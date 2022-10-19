a1ae0c01f2cab95812f3b189e1c7c252434baa1a
MarKr17 <mariakroczek199@gmail.com>
2022-10-19 15:17:28 +0200

    matrix build

--- Projekt1/Dotplot.py
+++ Projekt1/Dotplot.py
@@ -1,8 +1,26 @@
 import os
+import matplotlib.pyplot as plt
+import pandas as pd
 os.chdir(os.getcwd())
-file = open("Data/4YCO_D", "r")
-sequence = file.readline()
-brackets = file.readline()
+filename = "4YCO_D"
+file = open("Data/"+filename, "r")
+sequence = file.readline().strip()
+brackets = file.readline().strip()
 
+pairs = ["GC", "CG", "TA", "AT", "UA", "AU"]
 print(sequence)
 print(brackets)
+
+Y = list(sequence)
+X = list(sequence)
+matrix = [[0]*len(X)]*len(Y)
+for i in range(len(X)):
+    for j in range(len(Y)):
+        if X[i]+Y[j] in pairs:
+            matrix[i][j] = 1
+
+print(matrix)
+plt.scatter()
+plt.ylabel(filename)
+plt.xlabel(filename)
+plt.show()
